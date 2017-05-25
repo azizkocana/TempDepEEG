a8821aad99e3e1eefab1b9ff9d6b705423dbccdb
Aziz <akocanaogullari@ece.neu.edu>
2017-05-25 15:11:26 -0400

    #See 4: MLP Implementation

---
+++ RDat_Demo_MLP.py
@@ -0,0 +1,64 @@
+import chainer
+from rnn_classify import MultiLayerLSTM, Classifier
+from sklearn.model_selection import train_test_split
+import time
+import numpy as np
+from chainer import Variable, optimizers
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from TrainingSet import TrainRNN, TrainMLP
+import os
+import pickle
+import random
+import winsound
+
+""" Trains a Recurrent Neural Network
+    Args:
+        Requires time series, each time sample labelled.
+        Requires input and training parameters to be adjusted.
+    Return:
+        An RNN model. """
+
+""" Data should be three dimensional array
+    data: [num_samples x length of time signal x number of channels]
+    label: [num_samples x 1 x length of time signal]"""
+
+print_name = '\D12a3a-session2-MLP5'
+
+# As we are using reshape form Matrix dimensions should be as this
+samples_o = sio.loadmat('data.mat')
+labels_o = sio.loadmat('label.mat')
+targets_o = sio.loadmat('tar.mat')
+raw_samples = samples_o['data_m']
+raw_labels = np.concatenate(targets_o['target_m']).astype(np.int32)
+
+# Parameters
+num_layer = 20  # Number of LSTM layers (depth)
+size_layer = 50
+size_out = 2
+max_num_iter = 200
+test_size = .1
+
+MLP = TrainMLP(raw_samples, raw_labels, ratio_batch=0.2, test_size=test_size,
+               valid_size=0.1, num_layer=num_layer, max_num_iter=max_num_iter,
+               valid_epoch=2)
+
+if os.path.exists('models' + print_name + '.p'):
+    MLP.min_val_model = pickle.load(open("models" + print_name + ".p", "rb"))
+else:
+    MLP.train()
+    pickle.dump(MLP.min_val_model, open("models" + print_name + ".p", "wb"))
+    plt.figure()
+    plt.plot(np.arange(1, MLP.max_num_iter + 1), MLP.train_loss,
+             label='train loss')
+    plt.plot(np.arange(MLP.valid_epoch, MLP.max_num_iter + 1,
+                       step=MLP.valid_epoch, ), MLP.valid_loss,
+             label='valid loss')
+    plt.xlabel('iteration[number]')
+    plt.ylabel('loss[softmax cross entropy]')
+    plt.legend(loc='upper right', shadow=True)
+    plt.grid(True)
+    plt.savefig('.\FigDummy' + print_name + '_Train.pdf', format='pdf')
+
+MLP.test()
+print('Accuracy: %{}'.format(100 * MLP.acc[0]))
